ddc33209bbb241b9b244701e9cba9fd3656cf8f7
voidstrx <rexdiesel@mail.ru>
2024-12-19 10:32:51 +0300

    Update setup.php

--- setup.php
+++ setup.php
@@ -6,18 +6,12 @@ $data = json_decode($json, true);
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="w3.css">
     <title>Display Files and Play Video</title>
     <style>
-        body {
-            font-family: Arial, sans-serif;
-        }
-        #files {
-            background-color: #f0f0f0;
-            padding: 10px;
-        }
         #videoPlayer {
             display: none;
-            width: 80%;
+            width: 100%;
             height: auto;
             margin: 20px 0;
         }
@@ -25,45 +19,7 @@ $data = json_decode($json, true);
 		display: none;
 		}
     </style>
-</head>
-<body>
-    <h1>Видео в S:\98_tv</h1>
-    <div id="files">
-        <?php
 
-        if (isset($data['files']) && !empty($data['files'][0])) {
-            
-			
-			foreach ($data['files'][0] as $file) {
-				
-                echo '<button onclick="playVideo(\'' . $file . '\')">Play ' . $file . '</button><br>';
-            }
-			
-
-        } else {
-            echo 'No files found.';
-        }
-        ?>
-    </div>
-	
-	</div>
-		<div id="refresh">
-        <button id="refresh" onclick="refresh()">Обновить список видео</button>
-    </div>
-	
-    <div id="status">
-        <?php
-        echo 'Текущее видео на TV: ' . $data['current'];
-        ?>
-	</div>
-
-	
-    <video id="videoPlayer" controls></video>
-	
-	<div id="setCurrent">
-        <button id="setAsCurrentButton" onclick="setAsCurrent()">Отправить на TV</button>
-    </div>
-	
     <script>
         function playVideo(file) {
             const videoPlayer = document.getElementById('videoPlayer');
@@ -103,8 +59,7 @@ $data = json_decode($json, true);
                 alert('No video selected');
             }
         }
-		
-		
+
 		async function refresh() {
 				try {
                 const response = await fetch('server.php', {
@@ -132,5 +87,57 @@ $data = json_decode($json, true);
 				
         }
     </script>
+</head>
+<body class="w3-light-grey ">
+    <div class="w3-bar w3-top w3-black w3-large">
+        <button class="w3-bar-item w3-hover-green w3-left w3-black" id="setCurrent" onclick="setAsCurrent()">Отправить на TV</button>
+        <button class="w3-bar-item w3-hover-red w3-right w3-black"  id="refresh" onclick="refresh()">Обновить список видео</button>
+    </div>
+
+    <div class="w3-main" style="margin-top:43px;">
+
+
+
+    
+        <div id="status" class="w3-container w3-center w3-padding-16 w3-dark-gray"><h1>
+            <?php
+                echo 'Текущее видео на TV: ' . $data['current'];
+            ?></h1>
+	    </div>
+
+    <table id="files"  class="w3-table w3-striped w3-white">
+            <tr>
+                <th></th>
+            </tr>
+
+            
+        <?php
+        if (isset($data['files']) && !empty($data['files'][0])) {
+
+			foreach ($data['files'][0] as $file) {
+				
+                echo '<tr><td><button class="w3-bar-item" onclick="playVideo(\'' . $file . '\')">Play ' . $file . '</button></td></tr>';
+            }
+			
+        } else {
+            echo 'No files found.';
+        }
+        ?>
+        
+    </table>
+
+    <video id="videoPlayer" controls class="w3-center"></video>
+
+</div>
+</div>
+
+<div class="w3-padding w3-padding-48"></div>
+    <div class="w3-container w3-dark-grey w3-padding-32">
+    Видеофайлы находятся в S:&#47;98_tv
+    </div>
+
+    <footer class="w3-container w3-black">
+        <p>Powered by kcio-chuna</p>
+    </footer>
 </body>
 </html>
